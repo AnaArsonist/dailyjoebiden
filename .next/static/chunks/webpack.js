b24579d21996ca29f3dfadfcf04f80db1337472b
Ana <113212503+AnaArsonist@users.noreply.github.com>
2023-08-29 17:08:15 +0100

    Add metatags

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eeb1fae2b7afabbd"; }
+/******/ 		__webpack_require__.h = function() { return "93307761a87d6ac3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,28 @@ export default function Document() {
     <Html className="">
       <Head>
         <title className="">Daily Joe Biden</title>
+        <meta name="title" content="Daily Joe Biden" />
+        <meta name="description" content="Daily Joe Biden Quotes" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://dailyjoebiden.vercel.app" />
+        <meta property="og:title" content="Daily Joe Biden" />
+        <meta property="og:description" content="Daily Joe Biden Quotes" />
+        <meta
+          property="og:image"
+          content="https://dailyjoebiden.vercel.app/public/images/joeog.png"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://dailyjoebiden.vercel.app"
+        />
+        <meta property="twitter:title" content="Daily Joe Biden" />
+        <meta property="twitter:description" content="Daily Joe Biden Quotes" />
+        <meta
+          property="twitter:image"
+          content="https://dailyjoebiden.vercel.app/public/images/joeog.png"
+        />
+
         <body className="">
           <Main />
           <NextScript />
